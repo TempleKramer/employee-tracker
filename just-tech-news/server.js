1acfc408a48c2904815336888f35eb16ddcc8646
unknown <temple0715@gmail.com>
2022-09-01 15:08:34 -0500

    update

--- just-tech-news/server.js
+++ just-tech-news/server.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -19,15 +19,18 @@ const sess = {
   })
 };
 
-const hbs = exphbs.create({});
- 
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess));
 
 app.use(require('./controllers/'));
 
---
+++ pizza-hunt/models/Pizza.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+
+
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String
+    },
+    createdBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdatVal) => dateFormat(createdatVal)
+    },
+    size: {
+      type: String,
+      default: 'Large'
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
+
+// const Pizza = model('Pizza', PizzaSchema);
+const Pizza = require('./Pizza');
+const Comment = require('./Comment');
+const dateFormat = require('../utils/dateFormat');
+
+module.exports = { Pizza, Comment };
